fe79c6abdf1289bcf3e61c2d237564e913e94701
hirotada <hsasaki@redhat.com>
2017-02-21 11:16:06 +0900

    modified hello.php

--- hello.php
+++ hello.php
@@ -1,8 +1,9 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>OpenShift PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello OpenShift</p>'; ?> 
+ <?php echo "<p>$_SERVER['SERVER_ADDR']</p>"; ?>
  </body>
 </html>
